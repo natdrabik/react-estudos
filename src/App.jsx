d68277697357cec60b42c51eec69b38b10624c70
Nathalie Almeida <45579354+natdrabik@users.noreply.github.com>
2026-08-19 14:39:12 +0000

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Header from "./components/Header";
-import Navigation from "./components/Navigation";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
@@ -8,7 +7,6 @@ function App() {
   return (
     <>
       <Header />
-      <Navigation />
       <main>
         <About />
         <Projects />
