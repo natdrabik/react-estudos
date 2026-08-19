8a40de686f0dcfa75aa225e0dba0551ebe64f5a2
Nathalie Almeida <45579354+natdrabik@users.noreply.github.com>
2026-08-19 17:47:21 +0000

    hero

---
+++ src/componets/Projetos.jsx
@@ -0,0 +1,10 @@
+function Hero() { //cria a função do hero
+  return (
+    <section id="intro" class="test">
+    <h1>Nat<span class="h1a">halie</span> <span class="h1b">Drabik</span></h1>
+    <p>Front-End Developer com background em Design Gráfico, localizada em Curitiba, Paraná.</p>
+  </section>
+  );
+}
+
+export default Hero; //exporta a função do hero
---
+++ src/componets/Sobre.jsx
@@ -0,0 +1,16 @@
+function Sobre() { //cria a função 
+  return (
+    <section id="about">
+    <h2>Sobre mim</h2>
+    <div class="about-me">
+      <img src="assets/images/nath2.png" alt="Me"/>
+      <div class="about-me__txt">
+        <p>Formada em Design Gráfico pela PUC-PR em 2012, trabalho na área de Front-End <strong>há mais de 15 anos</strong>.</p>
+        <p>Atualmente, trabalho principalmente com WordPress, com foco no<strong> desenvolvimento, criação e organização de componentes reutilizáveis</strong>. Gosto de estruturar projetos de forma consistente, pensando em escalabilidade, manutenção e clareza de código.</p>
+      </div>
+    </div>
+  </section>
+  );
+}
+
+export default Sobre; //exporta a função 
