cda47504a7ed4a329b637031d144eaa63a19c51d
Nathalie Almeida <45579354+natdrabik@users.noreply.github.com>
2026-08-19 14:38:57 +0000

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
+      <Navigation />
+      <main>
+        <About />
+        <Projects />
+      </main>
+      <Footer />
     </>
   );
 }
